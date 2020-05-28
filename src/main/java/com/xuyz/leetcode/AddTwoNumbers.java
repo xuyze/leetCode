4fbeb37158f060dfd6f4368303ff3e95ee391486
Zeny <12510705@qq.com>
2020-05-28 23:27:06 +0800

    代码提交

--- src/main/java/com/xuyz/leetcode/AddTwoNumbers.java
+++ src/main/java/com/xuyz/leetcode/AddTwoNumbers.java
@@ -26,24 +26,24 @@ public class AddTwoNumbers {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode listNode = new ListNode(0);
         
-//        List<Integer> list1 = new ArrayList();
-//        list1.add(l1.val);
-//        while (l1.next.val>=0){
-//            list1.add(l1.next.val);
-//            l1 = l1.next;
-//        }
-//        List<Integer> list2 = new ArrayList();
-//        list2.add(l2.val);
-//        while (l2.next.val>=0){
-//            list2.add(l2.next.val);
-//            l2 = l2.next;
-//        }
-//        int len = list1.size()>=list2.size() ? list1.size() : list2.size();
-//        int ll1 = list1.size();
-//        int ll2 = list2.size();
-//        for(int i=0;i<len;i++){
-//            listNode.val = list1.get(ll1-1-i) +list2.get(ll2-1-i);
-//        }
+        List<Integer> list1 = new ArrayList();
+        list1.add(l1.val);
+        while (l1.next.val>=0){
+            list1.add(l1.next.val);
+            l1 = l1.next;
+        }
+        List<Integer> list2 = new ArrayList();
+        list2.add(l2.val);
+        while (l2.next.val>=0){
+            list2.add(l2.next.val);
+            l2 = l2.next;
+        }
+        int len = list1.size()>=list2.size() ? list1.size() : list2.size();
+        int ll1 = list1.size();
+        int ll2 = list2.size();
+        for(int i=0;i<len;i++){
+            listNode.val = list1.get(ll1-1-i) +list2.get(ll2-1-i);
+        }
         return listNode;
     }
 
@@ -58,7 +58,7 @@ public class AddTwoNumbers {
         l12.next = l13;
         l21.next = l22;
         l22.next = l23;
-        //addTwoNumbers(l11,l21);
+        addTwoNumbers(l11,l21);
         System.out.print(l11.next.val);
     }
 }
